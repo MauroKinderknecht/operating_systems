974f23322e5322669562ec5d675deecef70100fd
Mauro Kinderknecht <mauro.mkinderknecht@gmail.com>
2023-08-16 18:03:46 -0300

    add semaphores

--- threads/threads.py
+++ threads/threads.py
@@ -3,16 +3,29 @@ import threading
 import time
 import sys
 
-def print_process_id():
-    print(threading.current_thread(), os.getpid())
-    time.sleep(60)
+def read_shared_resource(name):
+    s.acquire()
+    print('Reading shared resource from %s' % name)
+    time.sleep(2)
+    s.release()
 
-def main(processes):
+
+def print_process_id():
+    name = threading.current_thread().name.split(' ')[0]
+    print(name, "PID:", os.getpid())
+    read_shared_resource(name)
+
+def main(processes, semaphores):
+    global s
+    s = threading.Semaphore(semaphores)
     for i in range(processes):
         threading.Thread(target=print_process_id).start()
 
 if __name__ == '__main__':
     n = 3
+    s = 2
     if len(sys.argv) > 1:
         n = int(sys.argv[1])
-    main(n)
+    if len(sys.argv) > 2:
+        s = int(sys.argv[2])
+    main(n, s)
